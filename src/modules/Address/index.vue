9b2d812f4214d291dd37a9c3c4e81839a6bf4172
gitzky <927926261@qq.com>
2022-06-09 19:10:50 +0800

    update

---
+++ src/modules/Address/index.vue
@@ -0,0 +1,21 @@
+<!-- 修建地址 -->
+<script setup lang="ts">
+const Address = {
+  addr: '聚贤人家',
+  addrType: '新小区',
+  peopleTypes: '普通|年轻人多|中年人|老年人多',
+  peopleNumber: '10000',
+  price: '20000每年',
+}
+
+</script>
+
+<template>
+  <div class="container">
+    <p>addr:{{ Address.addr }}</p>
+    <p>addrType:{{ Address.addrType }}</p>
+    <p>peopleTypes:{{ Address.peopleTypes }}</p>
+    <p>peopleNumber:{{ Address.peopleNumber }}</p>
+    <p>price:{{ Address.price }}</p>
+  </div>
+</template>
---
+++ src/modules/Settings/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const Description = "修建地址"
+</script>
+
+<template>
+  <div class="container"></div>
+</template>
