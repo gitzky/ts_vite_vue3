18e6e46a482728ad30b50f59a4e7a3d43dceb31c
gitzky <927926261@qq.com>
2022-06-22 16:35:31 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  // 配置前端服务地址和端口
+  server: {
+    host: '127.0.0.1',
+    port: 3000,
+    // 是否开启 https
+    https: false,
+  },
+  base: './',
 })
