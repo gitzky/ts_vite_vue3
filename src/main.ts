313225ec4177af81278d311edba63cab75ca8916
gitzky <927926261@qq.com>
2022-06-22 19:22:37 +0800

    up

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-const app = createApp(App)
-console.log(router)
-
-app.use(router)
-
-app.mount('#app')
+createApp(App).use(router).mount('#app')
